d31589b143cf50ca3057da3cb24c1f8b87deb642
madhu123428 <ch.madhusdhan28@gmail.com>
2026-01-03 22:41:47 +0530

    Landing Screen

--- config/schema.tsx
+++ config/schema.tsx
@@ -1,8 +1,19 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+import { integer, pgTable, varchar,date, json } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   credits: integer().notNull().default(5),
+});
+
+export const ProjectTable = pgTable("project", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  projectId: varchar().notNull(),
+  userInput: varchar().notNull(),
+  device: varchar({ length: 1024 }).notNull(),
+  createdOn: date().defaultNow(),
+  config: json(),
+  userId:varchar().references(() => usersTable.email).notNull(),
 });
